d4f0cd4b50086b80726bf1aa6490a12882471b0c
Maheshwaran <154659268+Mahesh7397@users.noreply.github.com>
2025-12-31 11:51:53 +0530

    Delete devflow-portfolio-v2 copy directory
    vghy

--- devflow-portfolio-v2 copy/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-
-import { type ClassValue, clsx } from "https://esm.sh/clsx@^1.2.1";
-import { twMerge } from "https://esm.sh/tailwind-merge@^2.2.1";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
